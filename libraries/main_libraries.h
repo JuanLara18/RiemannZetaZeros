8cd9910de9763ee5e4603851baa2bb03f559d9d7
julian <juojimenezca@unal.edu.co>
2020-05-06 02:01:22 -0500

    Riemann Zeta Function implemented

--- libraries/main_libraries.h
+++ libraries/main_libraries.h
@@ -1,5 +1,6 @@
 #include <complex>
 #include <cmath>
+#include <iostream>
 #include <boost/math/special_functions/bernoulli.hpp>
 
 using namespace std::complex_literals;
@@ -31,4 +32,46 @@ std::vector<float> bernoulli_numbers(int n){
   std::vector<float> res;
   boost::math::bernoulli_b2n<float>(0, n, std::back_inserter(res));
   return res;
+}
+
+std::complex<double> riemann_zeta(std::complex<double> s, int nu = 15){
+  /* This function calculates the value of the Riemann Zeta Function in s,
+  using the parameters N and nu for the approximation of the Euler's Summation
+  Method.
+  Input:
+    - s: complex number.
+    - nu: integer.
+  Output:
+    - zeta: complex number.
+  */
+  int N;
+  // Empiric bound
+  if (abs(s)<10){
+    N = 1000;
+  }
+  else{
+    N = abs(s)/4;
+  }
+  std::complex<double> zeta(0, 0);
+
+  for (int ii =1; ii<N; ii++){
+    zeta = zeta + pow(ii, -s);
+  }
+  zeta = zeta + pow(N, std::complex<double>(1,0)-s)/(s-std::complex<double>(1,0))
+  +pow(N,-s)/std::complex<double>(2,0);
+
+  std::vector<float> bn = bernoulli_numbers(nu+1);
+  for (int ii = 1; ii<(nu+1); ii++){
+    zeta = zeta + std::complex<double>(bn[ii],0)*semi_factorial(s,2*(ii-1))*
+    pow(N, -s-std::complex<double>(2*ii-1,0))/semi_factorial(std::complex<double>(1,0), 2*ii-1);
+  }
+  // This is the error in the approximation
+  double error = abs(semi_factorial(s,2*nu-1)*std::complex<double>(bn[nu],0)*
+  pow(N,-real(s)-2*nu+1)/semi_factorial(std::complex<double>(1,0),2*nu-1)/(real(s)+2*nu-1));
+  if (error >1){
+      std::cout << "Warning: the bound for the error is "<<error<<" which means"
+      <<" that the precission of these calculations is inadequate. You could try to"
+      << " change N and nu of the riemann_zeta function" <<std::endl;
+  }
+  return zeta;
 }
